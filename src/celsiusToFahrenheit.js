f32ef9fd056b63367a002905f41bad94b7c0f004
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-09-11 16:34:07 +0100

    added celsius converter

--- src/celsiusToFahrenheit.js
+++ src/celsiusToFahrenheit.js
@@ -1,1 +1,48 @@
-export const celsiusToFahrenheit = input => checkInputType(input) ? convertCelsius(input) : throwTypeError(typeErrors.numberCelsius);
+import { convertCelsius, round } from './utils/maths.js';
+import { validateInput, validateOptions } from './utils/validation.js';
+
+export function celsiusToFahrenheit(input, options) {
+
+  // Input validation:
+  validateInput(input);
+  // Default decimal place limit:
+  const defaultLimit = 1;
+
+  // Check for existence of options object:
+  if (options === undefined) {
+    console.log('Use case 3: ');
+    return round(convertCelsius(input), defaultLimit);
+  }
+
+  // Options object validation:
+  validateOptions(options);
+  // If options object is provided and successfully validated:
+  let { limit, showScale } = options;
+  // If a negative limit is provided, make it zero:
+  if (limit < 0) limit = 0;
+
+  /* Use cases / tests:
+
+  // See the validateInput function in validation.js for the following cases:
+  1. No input (throws an Error)
+  2. Incorrect input type, for example 'String' (throws an Error)
+
+  // See control flow section below for the following cases:
+  3. Just a number, no options object (returns a Number)
+  4. limit >= 0 && (showScale === undefined || !showScale) (returns a Number)
+  5. limit >= 0 && showScale (returns a String)
+  6. (limit === undefined || !limit) && showScale (returns a String)
+
+  */
+
+  if (limit >= 0 && (showScale === undefined || !showScale)) {
+    console.log('Use case 4: ');
+    return round(convertCelsius(input), limit);
+  } else if (limit >= 0 && showScale) {
+    console.log('Use case 5: ');
+    return round(convertCelsius(input), limit) + '°F';
+  } else if ((limit === undefined || !limit) && showScale) {
+    console.log('Use case 6: ');
+    return round(convertCelsius(input), defaultLimit) + '°F';
+  }
+};
--- src/index.js
+++ src/index.js
@@ -18,8 +18,9 @@ Thoughts on API:
 */
 
 import { fahrenheitToCelsius } from './fahrenheitToCelsius.js';
+import { celsiusToFahrenheit } from './celsiusToFahrenheit.js';
 
-// Tests:
+// Fahrenheit Tests:
 
 // 1.
 // const result1 = fahrenheitToCelsius();
@@ -30,18 +31,43 @@ import { fahrenheitToCelsius } from './fahrenheitToCelsius.js';
 // console.log(result2, typeof result2);
 
 // 3.
-const result3 = fahrenheitToCelsius(100);
+// const result3 = fahrenheitToCelsius(100);
+// console.log(result3, typeof result3);
+
+// 4.
+// const result4 = fahrenheitToCelsius(100, { limit: 0 });
+// console.log(result4, typeof result4);
+
+// 5.
+// const result5 = fahrenheitToCelsius(100, { limit: 2, showScale: true });
+// console.log(result5, typeof result5);
+
+// 6.
+// const result6 = fahrenheitToCelsius(100, { showScale: true });
+// console.log(result6, typeof result6);
+
+// Celsius Tests:
+
+// 1.
+// const result1 = celsiusToFahrenheit();
+// console.log(result1, typeof result1);
+
+// 2.
+// const result2 = celsiusToFahrenheit('abc');
+// console.log(result2, typeof result2);
+
+// 3.
+const result3 = celsiusToFahrenheit(23);
 console.log(result3, typeof result3);
 
 // 4.
-const result4 = fahrenheitToCelsius(100, { limit: 0 });
+const result4 = celsiusToFahrenheit(23, { limit: 0 });
 console.log(result4, typeof result4);
 
 // 5.
-const result5 = fahrenheitToCelsius(100, { limit: 2, showScale: true });
+const result5 = celsiusToFahrenheit(7.764, { limit: 2, showScale: true });
 console.log(result5, typeof result5);
 
 // 6.
-const result6 = fahrenheitToCelsius(100, { showScale: true });
+const result6 = celsiusToFahrenheit(23, { showScale: true });
 console.log(result6, typeof result6);
-
